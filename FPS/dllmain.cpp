4c0fe3bfce0dd7b66510594bba6ec40f425766c5
StargazingNeko <nekoheart93@gmail.com>
2018-09-18 13:38:43 -0700

    I did stuff, most of which is probably going to be changed, only pushing for backup.

--- FPS/dllmain.cpp
+++ FPS/dllmain.cpp
@@ -1,5 +1,53 @@
+#include <windows.h>
 #include <iostream>
-#include <Windows.h>
+#include <string>
+#include <sstream>
+#include <psapi.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <d3d10_1.h>
+#include <d3d10.h>
+#include <d3dx10.h>
+#include <d3d11.h>
+#include <d3d12.h>
+
+DWORD pid = GetCurrentProcessId();
+
+int ListModules(DWORD ProcId)
+{
+
+	std::stringstream ss;
+	ss << "PID: " << pid;
+
+	HMODULE hModule[1024];
+	HANDLE hProc;
+	DWORD cbNeeded;
+	unsigned int i;
+
+	hProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, ProcId);
+
+	if (EnumProcessModules(hProc, hModule, sizeof(hModule), &cbNeeded)) 
+	{
+		for (i = 0; i < (cbNeeded / sizeof(hModule)); i++)
+		{
+			std::cout << ss.str() << std::endl;
+			TCHAR szModName[MAX_PATH];
+			if (GetModuleFileNameEx(hProc, hModule[i], szModName, sizeof(szModName) / sizeof(TCHAR)))
+			{
+				_tprintf(TEXT("\t%s (0x%08X)\n)"), szModName, hModule[i]);
+			}
+			else
+			{
+				std::cout << "Error!" << std::endl;
+			}
+
+		}
+	}
+
+	CloseHandle(hProc);
+	return 0;
+}
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
@@ -10,7 +58,13 @@ BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 		//As of right now this is temporary for testing and learning injection! Will be replaced with overlay drawing which displays FPS in the future.//
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		MessageBoxA(0, "Injection Success!", "Success!", 0);
+		AllocConsole();
+		FILE* fp;
+		freopen_s(&fp, "CONOUT$", "w", stdout);
+		std::cout << "Injection Success!" << std::endl;
+
+		ListModules(pid);
+
 		return TRUE;
 	}
 	return FALSE;
