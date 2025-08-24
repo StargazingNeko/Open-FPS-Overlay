6136358fe553441fdc105c04217ad4b3844e7e71
StargazingNeko <StargazingNeko@users.noreply.github.com>
2025-08-24 16:32:43 -0700

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Open-FPS-Overlay
+## TO-DO:
+1. Add functionality to the "Bind Key" button to listen for the key that is to be used to enable the overlay drawing and dll injection
+2. Setup configuration file to store the key bindings and possible other future settings
+3. Game/Process hooking and dll injection
+4. DX/OpenGL overlay drawing
+5. Possibly hash verification of some kind so that you can't simply replace the dll with a cheat or some other malicous code
+6. Most likely more but I'm still studying on the needs for this
---
+++ dll/FPS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_FPS_Overlay
+{
+    public class FPS
+    {
+        private static int startTime;
+        private static int endTime;
+        private static int frameTimes = 0;
+        private static short frames = 0;
+
+    // Start counting the fps
+    public static void StartCounter()
+        {
+            //get the current time
+            int startTime = (int)TimeSpan.TicksPerMillisecond;
+        }
+
+        //stop counting the fps and display it at the 
+        public static void StopAndPost()
+        {
+        //get the current time
+        int endTime = (int)TimeSpan.TicksPerMillisecond;
+            //the difference between start and end times
+            frameTimes = frameTimes + endTime - startTime;
+        //count one frame
+        frames++;
+        //if the difference is greater than 1 second (or 1000ms) post the results
+        if(frameTimes >= 1000)
+        {
+                //post results at the console
+                Console.WriteLine(frames.ToString());
+                //reset time differences and number of counted frames
+                frames = 0;
+                frameTimes = 0;
+        }
+    }
+}
+}
