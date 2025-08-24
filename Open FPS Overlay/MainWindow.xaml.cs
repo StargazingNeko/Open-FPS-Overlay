e99ac9fccb252707c027e77661417c681cdba655
StargazingNeko <StargazingNeko@users.noreply.github.com>
2025-08-24 16:32:43 -0700

    Changed DLL portion to C++, added Newtonsoft for upcoming configuration file addition, testing keybinding related stuff(and failing at it).
    Mainly pushing this out to try to get some help with the project as I can't find much by googling.

--- Open FPS Overlay/MainWindow.xaml.cs
+++ Open FPS Overlay/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +22,16 @@ namespace Open_FPS_Overlay
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public int HOTKEY_ID { get; set; }
+
+        JObject config = JObject.Parse("config.json");
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +39,8 @@ namespace Open_FPS_Overlay
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Console.WriteLine("New Keybinding Requested");
+            
         }
     }
 }
