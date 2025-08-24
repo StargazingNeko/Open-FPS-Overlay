7ed153eaceb9fd8fc027a75820cf87a9ec68fb93
StargazingNeko <StargazingNeko@users.noreply.github.com>
2025-08-24 16:32:43 -0700

    Allows out of focus keybind pressing

--- Open FPS Overlay/MainWindow.xaml.cs
+++ Open FPS Overlay/MainWindow.xaml.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace Open_FPS_Overlay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     { 
         JavaScriptSerializer jss = new JavaScriptSerializer();
         KeyConverter convertKey = new KeyConverter();
-        
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9000;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -22,7 +25,7 @@ namespace Open_FPS_Overlay
             {
                 var defaultConfig = new Dictionary<string, dynamic>
                 {
-                    { "Hotkey", Key.F12 }
+                    { "Hotkey", Key.F11 }
                 };
                 File.WriteAllText("config.json", jss.Serialize(defaultConfig));
             }
@@ -37,5 +40,77 @@ namespace Open_FPS_Overlay
             Bound_Text.Text = "...";
             
         }
+
+        #region Register Hotkey
+        private void RegisterHotKey() {
+            var helper = new WindowInteropHelper(this);
+            const uint VK_F10 = 0x79;
+            const uint MOD_CTRL = 0x0002;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F10)) { // Handles error for Control + F10
+                // handle error
+            }
+        }
+        #endregion
+
+        #region Remove/unbind Hotkey
+        private void UnregisterHotKey() {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+        #endregion
+
+        #region Private Methods
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg) {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32()) {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed() {
+            Console.WriteLine("Binding pressed");
+        }
+        #endregion
+
+        #region Dll Imports
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id,
+            [In] uint fsModifiers,
+            [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        #endregion
+
+        #region Protected Overrides
+        protected override void OnSourceInitialized(EventArgs e) {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+        }
+
+        protected override void OnClosed(EventArgs e) {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
+
+        #endregion
     }
 }
